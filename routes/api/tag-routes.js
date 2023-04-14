c0fcae0501a43753343376d72eaaada57e945e58
cviguers <cviguers@gmail.com>
2023-04-14 16:40:56 -0400

    tag routes updated

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,28 +1,80 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
-// The `/api/tags` endpoint
+//  `/api/tags` endpoint
 
+// find all tags
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+  // call sequelize to findAll
+  Tag.findAll({ 
+    // include its associated Product data through ProductTag
+    include: [ 
+      { model: Product,  through: ProductTag, }
+    ],
+  })
+  // respond with found data
+  .then((categories) => res.json(categories))
+  // if err, throw err
+  .catch((err) => res.status(400).json(err));
 });
 
+// find a single tag by its `id`
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+  // call sequelize to findOne
+  Tag.findOne({
+    // look to the req.params to find tag by id
+    where: {
+      id: req.params.id,
+    },
+    // include its associated Product data through ProductTag
+    include: [ 
+    { model: Product,  through: ProductTag, }
+    ],
+  })
+  // respond with found data
+  .then((categories) => res.json(categories))
+  // if err, throw err
+  .catch((err) => res.status(400).json(err));
 });
 
+
+// create a new tag
 router.post('/', (req, res) => {
-  // create a new tag
+  // call sequelize to create what is in the request body
+  Tag.create(req.body)
+  // respond with found data
+  .then((categories) => res.json(categories))
+  // if err, throw err
+  .catch((err) => res.status(400).json(err));
 });
 
+// update a tag's name by its `id` value
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+  // call sequelize to update what is in the request body
+  Tag.update(req.body, { 
+    // targets by specific id
+    where:{
+      id: req.params.id
+    },
+  })
+  // respond with found data
+  .then((categories) => res.json(categories))
+  // if err, throw err
+  .catch((err) => res.status(400).json(err));
 });
 
+// delete on tag by its `id` value
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+  // call sequelize to delete selected tag
+  Tag.destroy({ 
+    where: {
+      id: req.params.id,
+    }
+  })
+  // respond with found data
+  .then((categories) => res.json(categories))
+  // if err, throw err
+  .catch((err) => res.status(400).json(err));
 });
 
 module.exports = router;
